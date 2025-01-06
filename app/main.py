4e1ac91a0a8f2f193b8e678cb6f9b3d1b82161af
Sungwoo Park_computer <sharkclass@naver.com>
2025-01-06 23:27:51 +0900

    study: Test simple api

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
+from typing import Annotated
+
+from datetime import datetime
 
 app = FastAPI()
 
@@ -6,3 +10,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+class StockPrice(BaseModel):
+    datetime: Annotated[datetime, Body()]
+    ticker: str
+    price: float
+
+
+@app.get("/stockprice", response_model=StockPrice)
+async def get_stock_price(ticker: str):
+
+    data = {
+        "datetime": datetime.now(),
+        "ticker": ticker,
+        "price": 1000,
+    }
+
+    stockprice = StockPrice(**data)
+
+    return stockprice
